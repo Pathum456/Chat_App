69685b7e796b5c1d32d22df1442eb333dc44cecd
pathum456 <gdse58courseonlinelessons2021@gmail.com>
2022-08-11 00:54:30 +0530

    client Form Created

--- src/controller/ClientFormController.java
+++ src/controller/ClientFormController.java
@@ -1,8 +1,24 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 /**
  * @author _ Pathum_Kaleesha 2022-08-10 - 22.07
  * @since - v0.1.0
  **/
 public class ClientFormController {
+    public ImageView ImgClientProfile;
+    public Label SeverNameLbl;
+    public JFXButton BtnSelectServer;
+    public ImageView imgSendChats;
+
+
+    public void chatServerOnclick(MouseEvent mouseEvent) {
+    }
+
+    public void sendChatsOnClick(MouseEvent mouseEvent) {
+    }
 }
