536dacb23eb3e47e176263d809d85fd04c86d816
pathum456 <gdse58courseonlinelessons2021@gmail.com>
2022-08-12 22:48:24 +0530

    Update

--- src/controller/ClientFormController.java
+++ src/controller/ClientFormController.java
@@ -1,9 +1,17 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 /**
  * @author _ Pathum_Kaleesha 2022-08-10 - 22.07
@@ -14,11 +22,38 @@ public class ClientFormController {
     public Label SeverNameLbl;
     public JFXButton BtnSelectServer;
     public ImageView imgSendChats;
+    public JFXTextField txtClientMessage;
+    public TextArea txtClientPane;
 
+    Socket socket=null;
+    public void initialize() throws IOException {
+        new Thread(()->{
+            try {
+                socket= new Socket("localhost",5000);
+                InputStreamReader inputStreamReader =
+                        new InputStreamReader(socket.getInputStream());
+                BufferedReader bufferedReader= new BufferedReader(inputStreamReader);
+                String record= bufferedReader.readLine();
+                System.out.println(record);
+                txtClientPane.appendText(record);
 
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+
+    }
     public void chatServerOnclick(MouseEvent mouseEvent) {
     }
 
-    public void sendChatsOnClick(MouseEvent mouseEvent) {
+    public void sendChatsOnClick(MouseEvent mouseEvent) throws IOException {
+        while(true) {
+            if (!txtClientMessage.getText().equals("exit")) {
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+                printWriter.println(txtClientMessage.getText());
+                txtClientPane.appendText(txtClientMessage.getText());
+                printWriter.flush();
+            }
+        }
     }
 }
--- src/controller/ServerFormController.java
+++ src/controller/ServerFormController.java
@@ -1,10 +1,26 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.TextFlow;
+
+import javax.xml.soap.Text;
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * @author _ Pathum_Kaleesha 2022-08-10 - 22.15
@@ -16,12 +32,46 @@ public class ServerFormController {
     public ImageView ImgClientProfile;
     public Label clientNameLbl;
     public JFXButton BtnSelectClient;
+    public TextArea txtServerPane;
+    public JFXTextField txtServerMassege;
+    public ImageView imgSendServerMessage;
+    Socket accept=null;
 
-    void initialize() {
 
+    public void initialize() {
+        new Thread(() -> {
+            try {
+                ServerSocket serverSocket= new ServerSocket(5000);
+                System.out.println("Server started!");
+                accept= serverSocket.accept();
+                System.out.println("Client Connected!");
+                InputStreamReader inputStreamReader =
+                        new InputStreamReader(accept.getInputStream());
+                BufferedReader bufferedReader= new BufferedReader(inputStreamReader);
+                String record= bufferedReader.readLine();
+                /* while(true){if(!record.equals("exit")){*/
+                     System.out.println(record);
+                     txtServerPane.appendText(record);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
-    public void chatClientOnAction(MouseEvent mouseEvent) {
+    public void chatClientOnAction(MouseEvent mouseEvent) throws IOException {
+
+    }
+    public void sendMessage(MouseEvent mouseEvent) throws IOException {
+        while(true) {
+            if (!txtServerMassege.getText().equals("exit")) {
+                PrintWriter printWriter = new PrintWriter(accept.getOutputStream());
+                printWriter.println(txtServerMassege.getText());
+                txtServerPane.appendText(txtServerMassege.getText());
+                printWriter.flush();
+            }
+        }
+
+
     }
 }
     
